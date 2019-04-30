e9948f67c87d2c8b449a5cf18d49502cbc90b3cf
Trent Lloyd <trent@lloyd.id.au>
2019-04-30 15:36:30 +0800

    fstore_op_latency.rb: fix partial size calc
    previously when I calculated the size/alignment of partial writes,
    a write under 4096 bytes would count both the start and end offset which
    doesn't make sense. Try to handle that better.

--- fstore_op_latency.rb
+++ fstore_op_latency.rb
@@ -14,6 +14,7 @@ slow_ops_time = 0
 total_ops = 0
 thread = {}
 
+partial_sizes = {}
 threshold = 0.2
 
 ARGF.each_line do |l|
@@ -35,6 +36,22 @@ ARGF.each_line do |l|
         wr_size = write_data[1].to_i
         start_partial = (wr_offset % 4096)
         end_partial = ((wr_offset + wr_size) % 4096)
+        start_block = wr_offset / 4096
+        end_block = (wr_offset + wr_size) / 4096
+        num_blocks = end_block - start_block + 1
+        if num_blocks == 1
+            if (start_partial > 0) && (end_partial > 0)
+                end_partial = 0
+            end
+        end
+        if start_partial > 0
+            partial_sizes[start_partial] ||= 0
+            partial_sizes[start_partial] += 1
+        end
+        if end_partial > 0
+            partial_sizes[end_partial] ||= 0
+            partial_sizes[end_partial] += 1
+        end
         thread[thread_id][:write] = {time: cur_t, line: l, start_partial: start_partial, end_partial: end_partial}
     end
     if l =~ / 15 filestore(.*) write .*/
@@ -59,7 +76,7 @@ ARGF.each_line do |l|
         if thread[thread_id][:write][:start_partial] > 0 || thread[thread_id][:write][:end_partial] > 0
             partial = true
             partial_ops += 1
-            #printf("0%1s%1s%1s%1s %.6f %.6f %.6f %s", delta_w > threshold ? "W" : " ", delta_ws > threshold ? "S" : " ", delta_f > threshold ? "F" : "", partial ? "P" : " ", delta_w, delta_ws, delta_f, thread[thread_id][:start][:line])
+            #printf("0%1s%1s%1s%1s %.6f %.6f %.6f %s", delta_w > threshold ? "W" : " ", delta_ws > threshold ? "S" : " ", delta_f > threshold ? "F" : "", partial ? "P" : " ", delta_w, delta_ws, delta_f, thread[thread_id][:write][:line])
         end
         if delta_w > threshold || delta_f > threshold || delta_ws > threshold
             slow_ops += 1
@@ -79,3 +96,4 @@ printf("%8s: %d\n", "Partial (non-4K) Ops", partial_ops)
 printf("%8s: %d\n", "Slow & Partial Ops", slow_partial_ops)
 printf("%8s: %d\n", "Total Ops", total_ops)
 printf("%8s: %d\n", "Slow Ops Time", slow_ops_time)
+pp partial_sizes
